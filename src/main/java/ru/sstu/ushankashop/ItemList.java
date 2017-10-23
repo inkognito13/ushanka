69d4fb47e160f7b656891bfbc423414d9c71d22a
Dmitriy Tarasov <gribnoydojdik@gmail.com>
2017-10-23 19:54:13 +0400

    Initial commit

---
+++ src/main/java/ru/sstu/ushankashop/ItemList.java
@@ -0,0 +1,29 @@
+package ru.sstu.ushankashop;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "itemList")
+@XmlAccessorType(XmlAccessType.NONE)
+public class ItemList {
+    @XmlElement
+    private List<Item> items;
+
+    public ItemList() {
+    }
+
+    public ItemList(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
